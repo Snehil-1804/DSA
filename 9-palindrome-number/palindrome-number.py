32f1df6183810ee849e70d5088eb9749aaf8abcc
Snehil Priyam <75743171+Snehil-1804@users.noreply.github.com>
2026-05-19 19:36:05 +0530

    Time: 21 ms (5.51%) | Memory: 19.3 MB (53.95%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num=x
+        result=0
+        while num>0:
+            id = num%10
+            result=(result*10)+id
+            num=num//10
+        if(result==x):
+            return True
+        else:
+            return False
