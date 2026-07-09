828ce2375b29704fde6d54b53028c658aa70854d
Snehil Priyam <75743171+Snehil-1804@users.noreply.github.com>
2026-07-09 22:44:53 +0530

    Time: 610 ms (69.93%) | Memory: 22.3 MB (31.86%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,12 +3,13 @@ class Solution:
         n=len(nums)
         ans=[]
         nums.sort()
-        my_set=set()
+
         for i in range(n):
-            if nums[i]!=0 and nums[i]==nums[i-1]:
+            if i!=0 and nums[i]==nums[i-1]:
                 continue
             j=i+1
             k=n-1
+
             while j<k:
                 total_sum=nums[i]+nums[j]+nums[k]
                 if total_sum>0:
@@ -16,12 +17,12 @@ class Solution:
                 elif total_sum<0:
                     j+=1
                 else:
-                    temp=[nums[i],nums[j],nums[k]]
-                    my_set.add(tuple(temp))
+                    ans.append([nums[i],nums[j],nums[k]])
                     j+=1
                     k-=1
                     while j<k and nums[j]==nums[j-1]:
                         j+=1
-                    while j<k and nums[k]== nums[k+1]:
+                    while j<k and nums[k]==nums[k+1]:
                         k-=1
-        return [list(ans) for ans in my_set]
+        return ans
+                
