3d82d7872707eac138c75222d5113971bf76b7ad
Snehil Priyam <75743171+Snehil-1804@users.noreply.github.com>
2026-07-14 20:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (86.21%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -7,5 +7,5 @@ class Solution:
             if not merged or merged[-1][1]<interval[0]:
                 merged.append(interval)
             else:
-                merged[-1]=[merged[-1][-0],max(merged[-1][-1],interval[1])]
+                merged[-1]=[merged[-1][0],max(merged[-1][-1],interval[1])]
         return merged
