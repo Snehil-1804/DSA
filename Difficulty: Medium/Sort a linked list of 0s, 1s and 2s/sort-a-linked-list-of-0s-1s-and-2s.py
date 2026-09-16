871188d9785ce771d6bfed2cee635dabc1ac0ce8
Snehil Priyam <75743171+Snehil-1804@users.noreply.github.com>
2026-09-16 12:59:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.py
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.py
@@ -7,20 +7,35 @@ class Node:
 	
 class Solution:
     def segregate(self, head):
-        #code here
-        res=[]
-        curr1=head
-        while curr1 is not None:
-            res.append(curr1.data)
-            curr1=curr1.next
-        res=sorted(res)
-
-        head_final=Node(res[0])
-        curr_final=head_final
-        for i in res[1:]:
-            curr_final.next=Node(i)
-            curr_final=curr_final.next
-
-
-
-        return head_final
+        if head is None or head.next is None:
+            return head
+        zero_head=Node(None)
+        one_head=Node(None)
+        two_head=Node(None)
+        zero=zero_head
+        one=one_head
+        two=two_head
+        
+        temp=head 
+        while temp is not None:
+            next_node = temp.next
+            temp.next = None
+            if temp.data==0:
+                zero.next=temp
+                zero=temp
+            elif temp.data==1:
+                one.next=temp
+                one=temp
+            else:
+                two.next=temp
+                two=temp
+            temp=next_node
+        if one_head.next is not None:
+            zero.next=one_head.next
+            one.next=two_head.next
+        else:
+            zero.next=two_head.next
+        # one.next=two_head.next
+        # two_head=None
+        
+        return zero_head.next
